bedf14770a345b830c376a4e214729197ce6089f
Glowhop <glowhop.temps@gmail.com>
2026-09-17 23:31:25 +0200

    feat: replace GlowTour components with GlowTourDefault for simplified usage

--- apps/playground/vue/PlaygroundTour.vue
+++ apps/playground/vue/PlaygroundTour.vue
@@ -1,22 +1,11 @@
 <script setup lang="ts">
 // biome-ignore lint/correctness/noUnusedImports: GlowTour is used in the template.
-import { GlowTour, type Tour } from "@glowhop/vue-tour";
+import { GlowTourDefault, type Tour } from "@glowhop/vue-tour";
 
 defineProps<{ tour: Tour }>();
 </script>
 
 <template>
-  <GlowTour.Root :tour="tour">
-    <GlowTour.Overlay />
-    <GlowTour.Pointer />
-    <GlowTour.Popover>
-      <GlowTour.Header />
-      <GlowTour.Content />
-      <GlowTour.Footer>
-        <GlowTour.PreviousTrigger />
-        <GlowTour.AdvanceTrigger />
-        <GlowTour.CancelTrigger />
-      </GlowTour.Footer>
-    </GlowTour.Popover>
-  </GlowTour.Root>
+  <GlowTourDefault :tour="tour" />
+    
 </template>
